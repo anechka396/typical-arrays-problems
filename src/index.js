f9dcbbcc03d5b0990b7938ff4e8ec9a899701fcf
Anna Yakubenko <ayakubenko@atypicaldigital.com>
2020-09-09 12:17:19 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+	if(array == null || array.length == 0)
+		return 0;
+	var min_value = array[0];
+	for(var i = 1; i < array.length; i++) {
+		if(min_value > array[i]) {
+			min_value = array[i];
+		}
+	}
+	return min_value;
 }
 
 exports.max = function max (array) {
-  return 0;
+	if(array == null || array.length == 0)
+		return 0;
+	var max_value = array[0];
+	for(var i = 1; i < array.length; i++) {
+		if(max_value < array[i]) {
+			max_value = array[i];
+		}
+	}
+	return max_value;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if(array == null || array.length == 0)
+		return 0;
+	var sum = 0;
+	for(var i = 0; i < array.length; i++) {
+		sum += array[i];
+	}
+	return sum / array.length;
 }
